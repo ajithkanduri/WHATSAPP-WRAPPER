f8d08b8f17e01d9de6a5655759754137c9d95ff2
ajithkanduri <37373929+ajithkanduri@users.noreply.github.com>
2024-02-08 17:06:08 -0500

    Update serverConfig.js

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,5 +1,5 @@
-module.exports.twilio_account_sid = 'AC186772b0322a1dd132ec855ce1c28b94'
-module.exports.twilio_auth_token = '77ee790d239826ae9f7af0f6724ae22a'
-module.exports.twilio_phone_number = '+14155238886'
-module.exports.sqs_url = 'https://sqs.ap-south-1.amazonaws.com/725657108007/medibuddydemo'
+module.exports.twilio_account_sid = '{{account_sid}}'
+module.exports.twilio_auth_token = '{{auth_token}}'
+module.exports.twilio_phone_number = '{{phone_number}}'
+module.exports.sqs_url = '{{sqs_url}}'
 module.exports.port = 3000
